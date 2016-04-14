7ddc77599442f002cf905b2c5192754ca68cb1c1
Richard Tan <rtan@verticalresponse.com>
2016-04-14 10:12:56 -0700

    whoops moved folder

---
+++ text/piglatin.py
@@ -0,0 +1,12 @@
+#Returns string in pig latin
+
+class StringMan():
+	def piglatin(self, string):
+
+
+
+if __name__ == '__main__':
+	x = StringMan()
+	string = input("Enter a string: ")
+	string = x.piglatin(string)
+	print (string)
